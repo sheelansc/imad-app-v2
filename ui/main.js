954ed28f3ea312c596d3360e7f83a6fead06f2b7
sheelansc <sheelan.sengupta@gmail.com>
2017-03-11 12:00:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,12 @@
 console.log('Loaded!');
 //move the image
 var img = document.getElementById('madi');
+var marginRight = 0;
+function moveLeft () {
+    marginRight = marginRight+ 10;
+    img.style.marginRight = marginRight + 'px'; 
+}
 img.onclick = function () {
+    var interval = setInterval(moveLeft,100);
     img.style.marginLeft = '100px';
 };
